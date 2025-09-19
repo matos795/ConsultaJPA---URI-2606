ab737028751fda9392b2cedb2561b4e0a5b0740f
Alexandre Matos <matosalexandre795@gmail.com>
2025-09-19 11:02:15 -0300

    Project Created

---
+++ src/main/java/com/consultaJPA/uri2606/Uri2606Application.java
@@ -0,0 +1,13 @@
+package com.consultaJPA.uri2606;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Uri2606Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Uri2606Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/consultaJPA/uri2606/Uri2606ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.consultaJPA.uri2606;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Uri2606ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
